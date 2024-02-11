747a85bb46bd51ca0b6e5a60c5382f32a7e2157a
Amy Pickup <amy.pickup@gmail.com>
2024-02-11 17:57:45 -0500

    completed handle-errors, https://go.dev/doc/tutorial/handle-errors

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,21 @@
 // collect related functions
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
 // Capitalized function names are "exported names"
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
+func Hello(name string) (string, error) {
+    // If no name was given, return an error with a message.
+    if name == "" {
+        return "", errors.New("empty name")
+    }
+
+    // If a name was received, return a value that embeds the name
+    // in a greeting message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
